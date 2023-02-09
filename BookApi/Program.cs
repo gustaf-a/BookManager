84cf62105278b991a8d643c3a8eff7da8e8983fb
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-09 21:08:52 +0100

    Add Startup-file to enable service tests

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,36 +1,18 @@
+namespace BookApi;
 
-namespace BookApi
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        var startup = new Startup();
+        startup.ConfigureServices(builder.Services);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            var app = builder.Build();
+        startup.Configure(app);
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
+        app.Run();
     }
 }
