50b4ba876e58da9289881d6b796c568ea191f37b
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-10 13:19:02 +0100

    Add Book data object

---
+++ BookApi/Data/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BookApi.Data;
+
+public class Book
+{
+    public string Id { get; set; }
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    public string Genre { get; set; }
+    
+    [JsonPropertyName("published_date")]
+    public DateTime PublishedDate { get; set; }
+}
