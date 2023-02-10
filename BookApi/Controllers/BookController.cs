cfc7c9e6e62a542e773a44bcaa07e3b757506fbb
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-10 11:53:47 +0100

    Add  BookController

---
+++ BookApi/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BookController : Controller
+    {
+        
+    }
+}
