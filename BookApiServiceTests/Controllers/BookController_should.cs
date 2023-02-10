1530567891ab5813129faeb3e69fbb68666a96ed
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-10 12:40:42 +0100

    [ServiceTests] Add BookController_should base for creating service tests

---
+++ BookApiServiceTests/Controllers/BookController_should.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using BookApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BookApiServiceTests.Controllers;
+
+public class BookController_should : IClassFixture<WebApplicationFactory<Startup>>
+{
+    private readonly HttpClient _client;
+
+    private const string ControllerBaseRoute = "api/books";
+
+    public BookController_should(WebApplicationFactory<Startup> factory)
+	{
+        _client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        //Add services to override with fake/mock services here
+                    });
+                }
+                )
+                .CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAllBooks_WhenBaseRouteCalled()
+    {
+        // Arrange
+
+        // Act
+        var responseStart = await _client.GetAsync($"{ControllerBaseRoute}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, responseStart.StatusCode);
+
+        //TODO Assert contents
+    }
+}
