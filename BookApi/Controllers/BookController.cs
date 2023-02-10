b8171ccadf9c50da9a5c99fb40c8d84c74a6cae2
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-10 13:20:02 +0100

    BaseRoute returns hardcoded collection of books

--- BookApi/Controllers/BookController.cs
+++ BookApi/Controllers/BookController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookApi.Data;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookApi.Controllers
+namespace BookApi.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("api/books")]
+public class BookController : Controller
 {
-    [ApiController]
-    [Route("api/books")]
-    public class BookController : Controller
+    [HttpGet]
+    public JsonResult GetAllBooks()
     {
-        
+        var books = new List<Book>
+        {
+            new Book{ Id = "B1", Author = "Testerson, TestName", Title = "Testers runbook to the galaxy", Description = "Boring book which tests your patience.", Genre = "Testing", Price = 1.99, PublishedDate = DateTime.Now.AddYears(-5) },
+            new Book{ Id = "B2", Author = "Testerson2, TestName2", Title = "Testers runbook to the galaxy 2", Description = "Boring new book which tests your patience again.", Genre = "Testing", Price = 3.99, PublishedDate = DateTime.Now.AddYears(-2) },
+        };
+
+        return Json(books);
     }
 }
