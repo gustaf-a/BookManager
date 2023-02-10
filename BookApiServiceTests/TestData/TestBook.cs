163f9a2c475fbe660fd698b039fb2917577b7321
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-10 19:08:41 +0100

    [ServiceTests] add TestBook object with attributes for json conversion

---
+++ BookApiServiceTests/TestData/TestBook.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BookApiServiceTests.TestData;
+
+internal class TestBook
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("price")]
+    [JsonConverter(typeof(DoubleWithDotJsonConverter))]
+    public double Price { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string Genre { get; set; }
+
+    //Attribute using custom JsonConverter needed to convert yyyy-MM-dd 
+    [JsonConverter(typeof(DateOnlyParseExactJsonConverter))] //JsonSerializer
+    //[JsonConverter(typeof(DateOnlyNewtonSoftJsonConverter))] //NewtonSoft
+    [JsonPropertyName("publish_date")]
+    public DateOnly PublishDate { get; set; }
+}
