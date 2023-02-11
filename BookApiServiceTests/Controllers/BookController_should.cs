27ed01d39ff661e250225b6d7725e14a856ec6aa
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-11 09:17:21 +0100

    [ServiceTests] add tests for getting all books sorted by the different fields

--- BookApiServiceTests/Controllers/BookController_should.cs
+++ BookApiServiceTests/Controllers/BookController_should.cs
@@ -37,7 +37,34 @@ public class BookController_should : IClassFixture<WebApplicationFactory<Startup
 
         // Assert
         responseStart.StatusCode.Should().Be(HttpStatusCode.OK);
-        
+
+        var content = await responseStart.Content.ReadAsStringAsync();
+
+        var booksCollection = JsonSerializer.Deserialize<List<TestBook>>(content);
+
+        booksCollection.Should().NotBeEmpty()
+            .And.BeEquivalentTo(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("B1 B2 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 B13", "id")]
+    [InlineData("B13 B3 B4 B8 B9 B1 B10 B2 B6 B12 B11 B7 B5", "author")]
+    [InlineData("B13 B8 B1 B11 B6 B3 B12 B10 B2 B4 B9 B7 B5", "title")]
+    [InlineData("B1 B10 B11 B12 B13 B2 B3 B4 B5 B8 B6 B7 B9", "genre")]
+    [InlineData("B6 B7 B8 B2 B3 B4 B9 B5 B13 B1 B11 B10 B12", "price")]
+    [InlineData("B5 B6 B7 B9 B3 B8 B10 B2 B4 B11 B12 B13 B1", "published")]
+    [InlineData("B7 B2 B11 B9 B3 B8 B1 B5 B4 B10 B13 B12 B6", "description")]
+    public async Task GetAllBooks_SortedByField_WithIdBeingFallbackSorting_WhenFieldIsAddedToRoute(string bookIdsToGet, string field)
+    {
+        // Arrange
+        var expectedResult = TestDataHelper.GetBooks(bookIdsToGet);
+
+        // Act
+        var responseStart = await _client.GetAsync($"{ControllerBaseRoute}/{field}");
+
+        // Assert
+        responseStart.StatusCode.Should().Be(HttpStatusCode.OK);
+
         var content = await responseStart.Content.ReadAsStringAsync();
 
         var booksCollection = JsonSerializer.Deserialize<List<TestBook>>(content);
