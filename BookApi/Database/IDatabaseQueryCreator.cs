5c67cd40a7fbad30d93f1ec9d0e0f393ec3ce131
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-12 13:42:17 +0100

    add IDatabaseQueryCreator

---
+++ BookApi/Database/IDatabaseQueryCreator.cs
@@ -0,0 +1,12 @@
+﻿using BookApi.Data;
+using BookApi.Database.SQLite;
+
+namespace BookApi.Database;
+
+public interface IDatabaseQueryCreator
+{
+    public string Create(Book book);
+    public SqlQuery Read(ReadBooksRequest readBooksRequest);
+    public string Update(Book book);
+    public string Delete(Book book);
+}
