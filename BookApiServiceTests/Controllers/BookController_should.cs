d83e0a6be909028e47563af513f7b8bff82960ff
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-12 13:52:15 +0100

    [ServiceTests] improve readability for GetAllBooks-tests

--- BookApiServiceTests/Controllers/BookController_should.cs
+++ BookApiServiceTests/Controllers/BookController_should.cs
@@ -49,11 +49,11 @@ public class BookController_should : IClassFixture<WebApplicationFactory<Startup
     [Theory]
     [InlineData("B1 B2 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 B13", "id")]
     [InlineData("B13 B3 B4 B8 B9 B1 B10 B2 B6 B12 B11 B7 B5", "author")]
-    [InlineData("B13 B8 B1 B11 B6 B3 B12 B10 B2 B4 B9 B7 B5", "title")]
+    [InlineData("B7 B2 B11 B9 B3 B8 B1 B5 B4 B10 B13 B12 B6", "description")]
     [InlineData("B1 B10 B11 B12 B13 B2 B3 B4 B5 B8 B6 B7 B9", "genre")]
     [InlineData("B6 B7 B8 B2 B3 B4 B9 B5 B13 B1 B11 B10 B12", "price")]
     [InlineData("B5 B6 B7 B9 B3 B8 B10 B2 B4 B11 B12 B13 B1", "published")]
-    [InlineData("B7 B2 B11 B9 B3 B8 B1 B5 B4 B10 B13 B12 B6", "description")]
+    [InlineData("B13 B8 B1 B11 B6 B3 B12 B10 B2 B4 B9 B7 B5", "title")]
     public async Task GetAllBooks_SortedByField_WithIdBeingFallbackSorting_WhenFieldIsAddedToRoute(string bookIdsToGet, string field)
     {
         // Arrange
@@ -68,6 +68,10 @@ public class BookController_should : IClassFixture<WebApplicationFactory<Startup
         var content = await responseStart.Content.ReadAsStringAsync();
 
         var booksCollection = JsonSerializer.Deserialize<List<TestBook>>(content);
+
+        //Asserting only by ID first to assert sorting and to improve readability of test failure message (hard to compare sorting order otherwise)
+        booksCollection.Select(b => b.Id).Should().NotBeEmpty()
+            .And.BeEquivalentTo(expectedResult.Select(e => e.Id), config => config.WithStrictOrdering());
 
         booksCollection.Should().NotBeEmpty()
             .And.BeEquivalentTo(expectedResult);
