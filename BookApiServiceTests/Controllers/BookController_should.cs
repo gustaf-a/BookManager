fc044889efa5e4a2fcf9ba9d611d900cefd9fd85
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-12 20:03:43 +0100

    [ServiceTests] add get books by text field filter tests

--- BookApiServiceTests/Controllers/BookController_should.cs
+++ BookApiServiceTests/Controllers/BookController_should.cs
@@ -54,13 +54,47 @@ public class BookController_should : IClassFixture<WebApplicationFactory<Startup
     [InlineData("B6 B7 B8 B2 B3 B4 B9 B5 B13 B1 B11 B10 B12", "price")]
     [InlineData("B5 B6 B7 B9 B3 B8 B10 B2 B4 B11 B12 B13 B1", "published")]
     [InlineData("B13 B8 B1 B11 B6 B3 B12 B10 B2 B4 B9 B7 B5", "title")]
-    public async Task GetAllBooks_SortedByField_WithIdBeingFallbackSorting_WhenFieldIsAddedToRoute(string bookIdsToGet, string field)
+    public async Task GetAllBooks_SortedByField_WhenFieldIsAddedToRoute(string bookIdsToGet, string field)
     {
         // Arrange
         var expectedResult = TestDataHelper.GetBooks(bookIdsToGet);
 
         // Act
         var responseStart = await _client.GetAsync($"{ControllerBaseRoute}/{field}");
+
+        // Assert
+        responseStart.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var content = await responseStart.Content.ReadAsStringAsync();
+
+        var booksCollection = JsonSerializer.Deserialize<List<TestBook>>(content);
+
+        //Asserting only by ID first to assert sorting and to improve readability of test failure message (hard to compare sorting order otherwise)
+        booksCollection.Select(b => b.Id).Should().NotBeEmpty()
+            .And.BeEquivalentTo(expectedResult.Select(e => e.Id), config => config.WithStrictOrdering());
+
+        booksCollection.Should().NotBeEmpty()
+            .And.BeEquivalentTo(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("B1 B2 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 B13", "id", "b")]
+    [InlineData("B1 B10 B11 B12 B13", "id", "b1")]
+    [InlineData("B1", "author", "joe")]
+    [InlineData("B1", "author", "kut")]
+    [InlineData("B1", "title", "deploy")]
+    [InlineData("B1", "title", "jruby")]
+    [InlineData("B1 B10 B11 B12 B13", "genre", "com")]
+    [InlineData("B1 B10 B11 B12 B13", "genre", "ter")]
+    [InlineData("B1 B13", "description", "deploy")]
+    [InlineData("B1", "description", "applications")]
+    public async Task GetBooks_FilteredBy_TextValues_SortedByField_WhenFieldIsAddedToRoute(string bookIdsToGet, string field, string filterValue)
+    {
+        // Arrange
+        var expectedResult = TestDataHelper.GetBooks(bookIdsToGet);
+
+        // Act
+        var responseStart = await _client.GetAsync($"{ControllerBaseRoute}/{field}/{filterValue}");
 
         // Assert
         responseStart.StatusCode.Should().Be(HttpStatusCode.OK);
