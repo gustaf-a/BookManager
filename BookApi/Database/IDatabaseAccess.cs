3ce2b1bc26d561eadf22988410f459d581375e7e
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-12 13:43:05 +0100

    add IDatabaseAccess

---
+++ BookApi/Database/IDatabaseAccess.cs
@@ -0,0 +1,11 @@
+﻿using BookApi.Data;
+
+namespace BookApi.Database;
+
+public interface IDatabaseAccess
+{
+    public Book CreateBook(Book book);
+    public IEnumerable<Book> ReadBooks(ReadBooksRequest readBooksRequest);
+    public Book UpdateBook(Book book);
+    public bool DeleteBook(Book book);
+}
