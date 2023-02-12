9e751a72c0ea463a00c61bdc82aa00b1f6825876
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-12 13:51:19 +0100

    start using DatabaseBookRepository in DI with SqliteDatabaseAccess and SqliteDatabaseQueryCreator

--- BookApi/Startup.cs
+++ BookApi/Startup.cs
@@ -1,4 +1,6 @@
-﻿using BookApi.Repositories;
+﻿using BookApi.Database;
+using BookApi.Database.SQLite;
+using BookApi.Repositories;
 using BookApi.Services;
 
 namespace BookApi;
@@ -8,7 +10,7 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-        
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(s =>
@@ -17,7 +19,10 @@ public class Startup
                 new Microsoft.OpenApi.Models.OpenApiInfo { Title = "BookApi", Version = "v1" });
         });
 
-        services.AddSingleton<IBookRepository, JsonFileBookRepository>();
+        services.AddSingleton<IBookRepository, DatabaseBookRepository>();
+        services.AddSingleton<IDatabaseAccess, SqliteDatabaseAccess>();
+        services.AddSingleton<IDatabaseQueryCreator, SqliteDatabaseQueryCreator>();
+
         services.AddSingleton<IBookService, BookService>();
     }
 
