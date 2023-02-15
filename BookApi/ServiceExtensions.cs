7fdb344648e82b311a83bb16952304cd73ebff62
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-15 15:42:06 +0100

    add ConfigureExceptionHandler extension method for global exception handling

--- BookApi/ServiceExtensions.cs
+++ BookApi/ServiceExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace BookApi;
+﻿using BookApi.Data;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace BookApi;
 
 public static class ServiceExtensions
 {
@@ -10,6 +14,30 @@ public static class ServiceExtensions
                 builder => builder.AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader());
+        });
+    }
+
+    public static void ConfigureExceptionHandler(this WebApplication app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    //TODO Log message
+
+                    await context.Response.WriteAsync(new ErrorDetails
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = $"Internal Server Error: {contextFeature.Error.Message}"
+                    }.ToString());
+                }
+            });
         });
     }
 }
