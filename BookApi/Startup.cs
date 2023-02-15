eb7ce6bfcf0e32a01964ddffa14f1af04a723d84
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-15 10:59:10 +0100

    add cors configuration

--- BookApi/Startup.cs
+++ BookApi/Startup.cs
@@ -11,6 +11,7 @@ public class Startup
     {
         services.AddControllers();
 
+        services.ConfigureCors();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
