db2446fe0f89160dd4ebf4eb5c3377370522e0ca
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-16 15:04:02 +0100

    add IRepositoryBase in Contracts

---
+++ Contracts/IRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts;
+
+public interface IRepositoryBase<T>
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="trackChanges">Greatly improves read speeds if set to false</param>
+    IQueryable<T> FindAll(bool trackChanges);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="trackChanges">Greatly improves read speeds if set to false</param>
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+    bool trackChanges);
+    
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
