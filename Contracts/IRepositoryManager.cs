7a458e4b6c7099f9ab7116499b0f91ec0766a083
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-16 15:04:39 +0100

    add IRepositoryManager to contracts

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts;
+
+/// <summary>
+/// Creates instances of repositories to facilitate handling multiple classes and controls when to save.
+/// Allows for multiple modifications with one save for performance and atomic transactions.
+/// </summary>
+public interface IRepositoryManager
+{
+    public IBookRepository Book { get; }
+
+    public void Save();
+}
