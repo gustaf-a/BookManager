732e358a2fea1e8a0b7bcf1accb56fddf6eb3492
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-19 14:28:27 +0100

    add ef core RepositoryManager

---
+++ RepositoryEFCore/RepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using Contracts.EF;
+
+namespace RepositoryEFCore;
+
+public sealed class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _repositoryContext;
+
+    private readonly Lazy<IBookEfRepository> _bookRepository;
+
+    public RepositoryManager(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+
+        _bookRepository = new Lazy<IBookEfRepository>(() => new BookEfRepository(repositoryContext));
+    }
+
+    public IBookEfRepository Book => _bookRepository.Value;
+
+    public void Save()
+        => _repositoryContext.SaveChanges();
+}
