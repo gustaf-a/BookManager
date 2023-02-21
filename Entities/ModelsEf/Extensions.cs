4d64c1466a14670e8e298e494af5d51a97047211
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-21 21:39:37 +0100

    add UpdateBookEf extension method to allow partial object updates

--- Entities/ModelsEf/Extensions.cs
+++ Entities/ModelsEf/Extensions.cs
@@ -55,4 +55,31 @@ public static class Extensions
             PublishDate = book.PublishDate.GetConvertedDateOnlyValue(),
             Title = book.Title
         };
+
+    public static void UpdateBookEf(this BookEf bookEf, BookEf bookEfUpdated)
+    {
+        if(NotDefault(bookEfUpdated.Author))
+            bookEf.Author = bookEfUpdated.Author;
+
+        if (NotDefault(bookEfUpdated.Description))
+            bookEf.Description = bookEfUpdated.Description;
+
+        if (NotDefault(bookEfUpdated.Genre))
+            bookEf.Genre = bookEfUpdated.Genre;
+
+        if (NotDefault(bookEfUpdated.Price))
+            bookEf.Price = bookEfUpdated.Price;
+
+        if (NotDefault(bookEfUpdated.PublishDate))
+            bookEf.PublishDate = bookEfUpdated.PublishDate;
+
+        if (NotDefault(bookEfUpdated.Title))
+            bookEf.Title = bookEfUpdated.Title;
+    }
+
+    private static bool NotDefault(string? value)
+        => !string.IsNullOrWhiteSpace(value);
+
+    private static bool NotDefault(double value)
+        => value > double.MinValue;
 }
