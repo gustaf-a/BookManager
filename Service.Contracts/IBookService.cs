e87abcfc12d5b2b75a3f4d908d458783a6234047
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2023-02-21 15:57:22 +0100

    fix typo in IBookService interface

--- Service.Contracts/IBookService.cs
+++ Service.Contracts/IBookService.cs
@@ -4,8 +4,8 @@ namespace Service.Contracts;
 
 public interface IBookService
 {
-    public Task<BookDto> CreateBook(BookDto BookDto);
+    public Task<BookDto> CreateBook(BookDto bookDto);
     public Task<IEnumerable<BookDto>> ReadBooks(ReadBooksRequest readBooksRequest);
-    public Task<bool> UpdateBook(BookDto BookDto, string bookId);
+    public Task<bool> UpdateBook(BookDto bookDto, string bookId);
     public Task<bool> DeleteBook(string bookId);
 }
